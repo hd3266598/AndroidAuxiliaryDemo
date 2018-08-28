9e8617e99d5ea4cf1c2acb72690e0cb737ffc688
984566961@qq.com <984566961@qq.com>
2018-08-28 16:35:17 +0800

    完整提交引用包

--- app/src/main/java/com/example/hd/androidauxiliarydemo/AccessibilitySampleService.kt
+++ app/src/main/java/com/example/hd/androidauxiliarydemo/AccessibilitySampleService.kt
@@ -1,11 +1,11 @@
 package com.example.hd.androidauxiliarydemo
 
 import android.accessibilityservice.AccessibilityService
+import android.accessibilityservice.AccessibilityServiceInfo
 import android.content.ServiceConnection
 import android.util.Log
 import android.view.accessibility.AccessibilityEvent
 import android.view.accessibility.AccessibilityNodeInfo
-
 
 
 /**
@@ -14,18 +14,23 @@ import android.view.accessibility.AccessibilityNodeInfo
  * @description
  */
 
- class AccessibilitySampleService : AccessibilityService() {
+class AccessibilitySampleService : AccessibilityService() {
+    override fun onCreate() {
+        super.onCreate()
+        serviceInfo.flags = AccessibilityServiceInfo.FLAG_REQUEST_TOUCH_EXPLORATION_MODE
+    }
+
     override fun onAccessibilityEvent(p0: AccessibilityEvent?) {
-        if (p0?.eventType== AccessibilityEvent.TYPE_VIEW_CLICKED) {
+        if (p0?.eventType == AccessibilityEvent.TYPE_VIEW_CLICKED) {
             val nodeInfo = rootInActiveWindow
-            dfsnode(nodeInfo, 0)
+            dfsNode(nodeInfo, 0)
         }
     }
 
 
     override fun onServiceConnected() {
         super.onServiceConnected()
-        Log.e(this.javaClass.simpleName,"吱吱吱~达达服务启动~")
+        Log.e(this.javaClass.simpleName, "吱吱吱~达达服务启动~")
     }
 
 
@@ -34,17 +39,17 @@ import android.view.accessibility.AccessibilityNodeInfo
 
     override fun unbindService(conn: ServiceConnection?) {
         super.unbindService(conn)
-        Log.e(this.javaClass.simpleName,"吱吱吱~达达服务关闭~")
+        Log.e(this.javaClass.simpleName, "吱吱吱~达达服务关闭~")
     }
 
-    private fun dfsnode(node: AccessibilityNodeInfo, num: Int) {
+    private fun dfsNode(node: AccessibilityNodeInfo, num: Int) {
         val stringBuilder = StringBuilder()
         for (i in 0 until num) {
             stringBuilder.append("__ ")    //父子节点之间的缩进
         }
         Log.i("####", stringBuilder.toString() + node.toString())   //打印
         for (i in 0 until node.childCount) {      //遍历子节点
-            dfsnode(node.getChild(i), num + 1)
+            dfsNode(node.getChild(i), num + 1)
         }
     }
 }
